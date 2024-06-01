5fca78c5b59cfa77eaa96eb2af098333c7a431a3
unknown <MFauzan29>
2024-06-02 00:53:23 +0700

    Setup Bootstrap

---
+++ retroactive-frontend/src/components/CartPage.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { MouseEvent, Fragment, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+
+function CartPage() {
+  const [getItemCount, setItemCount] = useState(0);
+}
