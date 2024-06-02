13cdabcbbf098636054440e1624375194c41a72b
unknown <MFauzan29>
2024-06-02 10:12:41 +0700

    Add Dummy Data

--- retroactive-frontend/src/App.jsx
+++ retroactive-frontend/src/App.jsx
@@ -9,8 +9,49 @@ import UserLoginPage from "./components/UserLoginPage";
 import CartPage from "./components/CartPage";
 
 function App() {
+  //Dummy Data
+  const discs = [
+    {
+      id: 1,
+      name: "Here Comes The Sun",
+      album: "Abbey Road",
+      band: "The Beatles",
+      year: "1970",
+    },
+    {
+      id: 2,
+      name: "Rocket Man",
+      album: "Honky Chateau",
+      band: "Elton John",
+      year: "1972",
+    },
+    {
+      id: 3,
+      name: "Dancing Queen",
+      album: "Arrival",
+      band: "The Beatles",
+      year: "1976",
+    },
+    {
+      id: 4,
+      name: "Bohemmian Rapsody",
+      album: "A Night at the Opera",
+      band: "Queen",
+      year: "1975",
+    },
+    {
+      id: 5,
+      name: "How Deep Is Your Love",
+      album: "Saturday Night Fever",
+      band: "Bee Gees",
+      year: "1977",
+    },
+  ];
+
   return (
     <>
+      {/* Import Dummy Data */}
+      <List items={discs} category="Discs" />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navigate to={"/home"} />} />
--- retroactive-frontend/src/components/CartPage.jsx
+++ retroactive-frontend/src/components/CartPage.jsx
@@ -5,8 +5,9 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
 
-function CartPage() {
+function CartPage(props) {
   const [getItemCount, setItemCount] = useState(0);
+  const itemList = props.items;
 
   let items = ["The Beatles", "Elton John", "ABBA", "Bee Gees", "Bon Jovi"];
 
@@ -15,25 +16,25 @@ function CartPage() {
   return (
     <>
       <h1 className="text-white">Cart List</h1>
-      {items.length === 0 && (
+      {itemList.length === 0 && (
         <p className="text-red-600 text-2xl bg-white">
           Anda Belum Memasukkan Barang Apapun!
         </p>
       )}
       <ul className="list-group bg-white shadow-md rounded-3xl px-8 pt-6 pb-8 mb-4">
-        {items.map((item, index) => (
+        {itemList.map((item, index) => (
           <li
             className={
               selectedIndex === index
                 ? "list-group-item active"
                 : "list-group-item"
             }
-            key={item}
+            key={item.id}
             onClick={() => {
               setSelectedIndex(index);
             }}
           >
-            {item}
+            {item.name} : &nbsp;
           </li>
         ))}
       </ul>
