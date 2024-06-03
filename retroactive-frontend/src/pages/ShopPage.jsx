88cca5c4157b27b77a2565d2e7e63d81c39fb50c
MFauzan29 <mfauzan2905@gmail.com>
2024-06-03 22:40:48 +0700

    Adding interface logo

--- retroactive-frontend/src/pages/ShopPage.jsx
+++ retroactive-frontend/src/pages/ShopPage.jsx
@@ -2,12 +2,17 @@ import React from "react"; // Importing the React library
 import { Hero } from "../components/hero/Hero"; // Importing the Hero component from the specified path
 import Popular from "../components/popular/PopularPage"; // Importing the Popular component from the specified path
 import AboutUs from "../components/about-us/AboutUsPage"; // Importing the AboutUs component from the specified path
+import logoretroactive from "../assets/logo.png";
 
 // Defining the Shop functional component
 export const Shop = () => {
   return (
     <div>
-      {/* <Hero /> */}
+      <img
+        src={logoretroactive}
+        className="flex justify-center items-center"
+      ></img>
+      <Hero />
       <Popular /> {/*Rendering the Popular component*/}
       {/* <AboutUs /> */}
     </div>
