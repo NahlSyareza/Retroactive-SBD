0e1e50417f25c87d8e403c1b84ac417b30be1249
Phoebe Ivana <phoebe.i.solaiman@gmail.com>
2024-06-09 20:29:50 +0700

    Fixing Errors

--- retroactive-backend/routes/UserRoute.js
+++ retroactive-backend/routes/UserRoute.js
@@ -2,26 +2,17 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
 
-// Session management routes
 router.post("/session", userController.loginEvent); // Endpoint for user login.
-router.post("/register", userOwner.registerEvent); // Endpoint for user registration.
-
-// User data retrieval and manipulation routes
+router.post("/register", userController.registerEvent); // Endpoint for user registration.
 router.get("/getAll", userController.getAllEvent); // Retrieve all user data.
 router.put("/edit", userController.editEvent); // Edit an existing user's details.
 router.get("/get", userController.getEvent); // Get specific user data.
-router.put("/topup", userBlabber.topUpEvent); // Top-up user account balance.
-
-// Inventory management
-router.post("/inventory", userCrypter.inventoryFunction); // Manage user inventory.
-router.put("/addToInventory", userCurator.addToInventory); // Add items to user inventory.
-
-// Financial and game interactions
-router.put("/gacor", userController.gacorEvent); // Endpoint for processing gambling interactions.
-router.put("/pay", userAlerter.payDipEvent); // Process payments.
-
-// User account management
+router.put("/topup", userController.topUpEvent); // Top-up user account balance.
+router.post("/inventory", userController.inventoryFunction); // Manage user inventory.
+router.put("/gacor", userController.gacorEvent); // Gacor event.
+router.put("/pay", userController.payEvent); // Doing the payment.
+router.put("/addToInventory", userController.addToInventory); // Endpoint for processing gambling interactions.
 router.delete("/delete", userController.deleteEvent); // Delete a user account.
-
 // Export the router to be used in other parts of the application.
+
 module.exports = router;
